3f69e17bbc84c95b9d0df445cd9a2e3698e86170
seedevk8s <seedevk8s@gmail.com>
2024-04-13 08:58:49 +0000

    first commit

---
+++ email-service/src/test/java/co/kr/ideacube/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.ideacube.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
